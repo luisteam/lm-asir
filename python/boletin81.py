e512fce9c076ca5f3ca93b16e9e21b9c670fe19f
Luis Raya <luis_rayita@hotmail.es>
2018-01-26 19:15:55 +0100

    Arreglos

--- python/boletin81.py
+++ python/boletin81.py
@@ -1,15 +1,15 @@
 #pasar_a_segundos
 
-def pasar_a_segundos(cadena):
-	horas=0
-	minutos=0
-	segundos=0
+def pasar_a_segundos(horas,minutos,segundos):
+	#horas=0
+	#minutos=0
+	#segundos=0
 
-	tiempo=cadena.split(":")
-	horas=int(tiempo[0])
-	minutos=int(tiempo[1])
-	segundos=int(tiempo[2])
+	#tiempo=cadena.split(":")
+	#horas=int(tiempo[0])
+	#minutos=int(tiempo[1])
+	#segundos=int(tiempo[2])
 
 	resultado=(((horas*60)+minutos)*60)+segundos
 
-	print(resultado)
+	return(resultado)
