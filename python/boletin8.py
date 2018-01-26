5fa1f7f870e97e2e4107d6f3bbec7019d8a25e84
Luis Raya <luis_rayita@hotmail.es>
2018-01-26 19:15:39 +0100

    Error fila 26

--- python/boletin8.py
+++ python/boletin8.py
@@ -1,5 +1,30 @@
 from boletin81 import pasar_a_segundos
 from boletin82 import calcular_coste
+from boletin83 import convertir_a_euros
 
-segundos=pasar_a_segundos("03:03:25")
-calcular_coste(int(segundos),15)
+coste=[]
+Lllamadas=[]
+total=[]
+
+tarifa=input("Tarifa centimos por segundo: ")
+llamadas=input("Numero de llamadas:" )
+
+for i in range(int(llamadas)):
+	horas=int(input("Horas llamada %d: " % (i)))
+	minutos=int(input("Minutos llamada %d: " % (i)))
+	segundos=int(input("Segundos llamada %d: " % (i)))
+	csegundos=pasar_a_segundos(horas,minutos,segundos)
+	centimos=calcular_coste(csegundos,tarifa)
+	euros=convertir_a_euros(centimos)
+	coste.append(i)
+	coste.append(euros)
+	Lllamadas.append(coste)
+	total.append(euros)
+	coste=[]
+
+for i in range(len(Lllamadas)):
+	print("La llamada %d cuesta %f Euros" % (Lllamadas[i,0],Lllamadas[i,1]))
+
+
+#3 3 25
+#1650.75 Euros
